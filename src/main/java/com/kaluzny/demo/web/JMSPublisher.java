ea7a1e910bc4d4b914e025f7bf104bcceb8b5b1c
Vladyslav <vladsw764@icloud.com>
2023-08-23 17:45:54 +0300

    remove JMSPublisher

--- src/main/java/com/kaluzny/demo/web/JMSPublisher.java
+++
@@ -1,9 +0,0 @@
-package com.kaluzny.demo.web;
-
-import com.kaluzny.demo.domain.Automobile;
-import org.springframework.http.ResponseEntity;
-
-public interface JMSPublisher {
-
-    ResponseEntity<Automobile> pushMessage(Automobile automobile);
-}
