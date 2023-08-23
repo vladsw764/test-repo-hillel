b563efd81810c068da0740b683f0b68bb05911f1
Vladyslav <vladsw764@icloud.com>
2023-08-23 17:45:26 +0300

    added some jms listeners

--- src/main/java/com/kaluzny/demo/listener/Consumer.java
+++ src/main/java/com/kaluzny/demo/listener/Consumer.java
@@ -1,9 +1,12 @@
 package com.kaluzny.demo.listener;
 
 import com.kaluzny.demo.domain.Automobile;
+import com.kaluzny.demo.dto.AutoResponseDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Slf4j
 @Component
@@ -21,6 +24,31 @@ public class Consumer {
 
     @JmsListener(destination = "AutoTopic", containerFactory = "automobileJmsContFactory")
     public void getAutomobileListener3(Automobile automobile) {
-        log.info("\u001B[34m" + "Automobile Consumer 3: " + automobile + "\u001B[0m");
+        log.info("\u001B[34m" + "Automobile Consumer 4: " + automobile + "\u001B[0m");
+    }
+
+    @JmsListener(destination = "AutoTopicList", containerFactory = "automobileJmsContFactory")
+    public void getAutomobileListener4(List<AutoResponseDto> automobile) {
+        log.info("\u001B[34m" + "Automobile Consumer 5: " + automobile + "\u001B[0m");
+    }
+
+    @JmsListener(destination = "AutoTopicList", containerFactory = "automobileJmsContFactory")
+    public void getAutomobileListener5(List<AutoResponseDto> automobile) {
+        log.info("\u001B[34m" + "Automobile Consumer 6: " + automobile + "\u001B[0m");
+    }
+
+    @JmsListener(destination = "AutoTopicList", containerFactory = "automobileJmsContFactory")
+    public void getAutomobileListener6(List<AutoResponseDto> automobile) {
+        log.info("\u001B[34m" + "Automobile Consumer 7: " + automobile + "\u001B[0m");
+    }
+
+    @JmsListener(destination = "AutoTopicList", containerFactory = "automobileJmsContFactory")
+    public void getAutomobileListener7(List<AutoResponseDto> automobile) {
+        log.info("\u001B[34m" + "Automobile Consumer 8: " + automobile + "\u001B[0m");
+    }
+
+    @JmsListener(destination = "AutoTopicList", containerFactory = "automobileJmsContFactory")
+    public void getAutomobileListener8(List<AutoResponseDto> automobile) {
+        log.info("\u001B[34m" + "Automobile Consumer 9: " + automobile + "\u001B[0m");
     }
 }
