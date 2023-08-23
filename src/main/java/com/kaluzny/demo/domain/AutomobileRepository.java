4796ff8623c5875b0836479b82c21d81e005905c
Vladyslav <vladsw764@icloud.com>
2023-08-23 17:44:27 +0300

    update AutomobileRepository

--- src/main/java/com/kaluzny/demo/domain/AutomobileRepository.java
+++ src/main/java/com/kaluzny/demo/domain/AutomobileRepository.java
@@ -1,18 +1,47 @@
 package com.kaluzny.demo.domain;
 
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Range;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Repository
 public interface AutomobileRepository extends JpaRepository<Automobile, Long> {
+
+    @Query(value = """
+            SELECT * FROM automobile WHERE deleted IS FALSE
+            """, nativeQuery = true)
+    List<Automobile> findAllExists();
+
+    @Transactional
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Query(value = "UPDATE automobile SET deleted = TRUE WHERE id = ?1", nativeQuery = true)
+    void markRemoved(UUID id);
+
+    @Query(value = """
+            UPDATE automobile SET name = :name, color = :color, original_color = :originalColor WHERE id = :id
+            """, nativeQuery = true)
+    Automobile updateAutomobile(String name, String color, Boolean originalColor, UUID id);
+
+    @Query(value = "SELECT * FROM automobile WHERE name = :name", nativeQuery = true)
     List<Automobile> findByName(String name);
 
+    @Query(value = "SELECT * FROM automobile WHERE color = :color", nativeQuery = true)
     List<Automobile> findByColor(String color);
 
+    @Query(value = "SELECT * FROM automobile WHERE name = :name AND color = :color", nativeQuery = true)
     List<Automobile> findByNameAndColor(String name, String color);
 
+    @Query(value = "SELECT * FROM automobile WHERE color LIKE :colorStartWith%", nativeQuery = true)
     List<Automobile> findByColorStartsWith(String colorStartWith, Pageable page);
+
+    @Query(value = "SELECT * FROM automobile WHERE id = :id", nativeQuery = true)
+    Optional<Automobile> findById(UUID id);
 }
